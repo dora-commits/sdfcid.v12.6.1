0601024bd40bdacb688f83641b64b232c11bb387
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-03-11 20:11:03 +0700

    added convolutional autoencoder on mnist 28x28

---
+++ autoencoder/convolutional_ae_mnist.py
@@ -0,0 +1,58 @@
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super(Encoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.n_channel, self.dim_h, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h, self.dim_h * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 2, self.dim_h * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 4, self.dim_h * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 8),
+            nn.LeakyReLU(0.2, inplace=True))
+
+        # final layer is fully connected
+        self.fc = nn.Linear(self.dim_h * (2 ** 3), self.n_z)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.squeeze()
+        x = self.fc(x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # first layer is fully connected
+        self.fc = nn.Sequential(
+            nn.Linear(self.n_z, self.dim_h * 8 * 7 * 7),
+            nn.ReLU())
+
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(self.dim_h * 8, self.dim_h * 4, 4),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 4, self.dim_h * 2, 4),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 2, self.n_channel, 4, stride=2),
+            nn.Tanh())
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, self.dim_h * 8, 7, 7)
+        x = self.deconv(x)
+        return x
