cb735afc41c26be73fa52c11bfb55695a8b45a6a
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-03-11 20:51:21 +0700

    added SMOTE into utils

--- utils/utils_mnist.py
+++ utils/utils_mnist.py
@@ -70,3 +70,57 @@ def SMOTE(X, y,n_to_sample,cl):
 
     #use 10 as label because 0 to 9 real classes and 1 fake/smoted = 10
     return samples, [cl]*n_to_sample
+
+# TODO : ADASYN Algorithm
+def ADASYN(X, y,xclass, yclass,  cl, m_major, m_minor, beta=1):
+  K = 5
+  d= m_major/m_minor
+  G = (m_major-m_minor)*beta
+
+  # fitting the model
+  clf = neighbors.KNeighborsClassifier()
+  clf.fit(X, y)
+  Ri = []
+  Minority_per_xi = []
+
+  for i in range(m_minor):
+    # Returns indices of the closest neighbours, and return it as a list
+    xi = xclass[i, :].reshape(1, -1)
+    # Returns indices of the closest neighbours, and return it as a list
+    neighbours = clf.kneighbors(xi, n_neighbors=K, return_distance=False)[0]
+    delta=0
+    for j in neighbours:
+      if(y[j]!=0):
+        delta+=1
+    Ri.append(delta/K)
+
+    minority = []
+    for index in neighbours:
+            # Shifted back 1 because indices start at 0
+            if y[index]==cl:
+                minority.append(index)
+    Minority_per_xi.append(minority)
+
+  Ri_norm = []
+  for ri in Ri:
+    ri_norm = ri / sum(Ri)
+    Ri_norm.append(ri_norm)
+
+  Gi = []
+  for r in Ri_norm:
+    gi = round(r * G)
+    Gi.append(int(gi))
+  syn_data=[]
+  syn_number =0
+
+  for i in range(m_minor):
+    neighbor_indices = np.random.choice(list(range(1, K+1)),Gi[i])
+    for j in range(Gi[i]):
+        # If the minority list is not empty
+        if Minority_per_xi[i]:
+            index = np.random.choice(Minority_per_xi[i])
+            xzi = X[index, :].reshape(1, -1)
+            si = xi + (xzi - xi) * np.random.uniform(0, 1)
+            syn_data.append(si)
+            syn_number+=1
+  return syn_data, [cl]*syn_number
