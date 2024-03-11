49fe0778dcae55ff003f26f7f86d091c63a1f538
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-03-11 18:45:17 +0700

    added arguments on mnist

---
+++ arguments/args_mnist.py
@@ -0,0 +1,31 @@
+# TODO : Arguments
+args = {}
+args['dim_h']                 = 64                    # factor controlling size of hidden layers
+args['n_channel']             = 1                     # number of channels in the input data
+args['n_z']                   = 300                   # number of dimensions in latent space.
+args['lr']                    = 0.0002                # learning rate for Adam optimizer .0001
+args['epochs']                = 50                    # how many epochs to run for
+args['batch_size']            = 100                   # batch size for SGD, Adam
+args['save']                  = True                  # save weights at each epoch of training if True
+args['train']                 = True                  # train networks if True, else load networks from
+args['resnet_learning_rate']  = 0.001                 # learning rate for Adam optimizer .001 with resnet18
+args['resnet_num_epochs']     = 30                    # how many epochs to run for
+args['oversampling_method']   = 'SMOTE'               # Oversampling Algorithm
+args['data_name']             = 'MNIST'               # Dataset
+args['header']                = True
+
+# TODO : Measuring Metrics
+num_repeats = 1
+Accuracy      = np.zeros(num_repeats)
+GeometricMean = np.zeros(num_repeats)
+F1Score       = np.zeros(num_repeats)
+Precision     = np.zeros(num_repeats)
+Recall        = np.zeros(num_repeats)
+
+result_df = pd.DataFrame(columns=['Accuracy', 'GeometricMean', 'F1Score', 'Precision', 'Recall'])
+
+# TODO : Check overfitting problem
+train_loss_total = []
+validation_loss_total = []
+train_acc_total = []
+validation_acc_total = []
