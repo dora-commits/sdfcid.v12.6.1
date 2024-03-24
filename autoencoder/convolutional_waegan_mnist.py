cc03f313293319f87bb6bb6e62b2de7dbb03734a
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-03-24 11:59:11 +0700

    added convolutional waegan

---
+++ autoencoder/convolutional_waegan_mnist.py
@@ -0,0 +1,103 @@
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super(Encoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # convolutional filters, work excellent with image data
+        # Using Leaky ReLU and Tanh activation function to handle Nan value of train loss when use MSELoss
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.n_channel, self.dim_h, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h, self.dim_h * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 2, self.dim_h * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 4, self.dim_h * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 8),
+            nn.LeakyReLU(0.2, inplace=True))
+
+        # final layer is fully connected
+        self.fc = nn.Linear(self.dim_h * (2 ** 3), self.n_z)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.squeeze()
+        x = self.fc(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # first layer is fully connected
+        self.fc = nn.Sequential(
+            nn.Linear(self.n_z, self.dim_h * 8 * 7 * 7),
+            nn.ReLU()
+        )
+
+        # deconvolutional filters, essentially the inverse of convolutional filters
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(self.dim_h * 8, self.dim_h * 4, 4),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 4, self.dim_h * 2, 4),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 2, self.n_channel, 4, stride=2),
+            # Using Tanh instead of Sigmoid
+            nn.Tanh()
+            # nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, self.dim_h * 8, 7, 7)
+        x = self.deconv(x)
+        return x
+
+# define the descriminator
+class Discriminator(nn.Module):
+    def __init__(self, args):
+        super(Discriminator, self).__init__()
+
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # main body of discriminator, returns [0,1]
+        self.main = nn.Sequential(
+            nn.Linear(self.n_z, self.dim_h * 4),
+            nn.ReLU(True),
+            nn.Linear(self.dim_h * 4, self.dim_h * 4),
+            nn.ReLU(True),
+            nn.Linear(self.dim_h * 4, self.dim_h * 4),
+            nn.ReLU(True),
+            nn.Linear(self.dim_h * 4, self.dim_h * 4),
+            nn.ReLU(True),
+            nn.Linear(self.dim_h * 4, 1),
+            # Using Tanh instead of Sigmoid
+            # nn.Tanh()
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.main(x)
+        return x
+
+# control which parameters are frozen / free for optimization
+def free_params(module: nn.Module):
+    for p in module.parameters():
+        p.requires_grad = True
+
+def frozen_params(module: nn.Module):
+    for p in module.parameters():
+        p.requires_grad = False
