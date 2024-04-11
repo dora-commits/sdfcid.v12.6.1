5106abdc9de2fdbc508994f3152379666e1bcc03
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-04-11 22:11:33 +0700

    updated arguments with rgb

--- arguments/args_cifar10.py
+++ arguments/args_cifar10.py
@@ -1,18 +1,19 @@
 # TODO : Arguments
 args = {}
 args['dim_h']                 = 64                    
-args['n_channel']             = 3                     
+args['n_channel']             = 3                     # number of channels in the input data
 args['n_z']                   = 600                   # number of dimensions in latent space.
-args['lr']                    = 0.0002                # learning rate for Adam optimizer .0002
-args['epochs']                = 50                   
+args['lr']                    = 0.0002                # learning rate for Adam optimizer .0001
+args['epochs']                = 50                    
 args['batch_size']            = 100                   # batch size for SGD, Adam
 args['save']                  = True                  # save weights at each epoch of training if True
 args['train']                 = True                  # train networks if True, else load networks from
 args['resnet_learning_rate']  = 0.001                 # learning rate for Adam optimizer .001 with resnet18
-args['resnet_num_epochs']     = 20                    
+args['resnet_num_epochs']     = 30                    
 args['oversampling_method']   = 'ADASYN'              # Oversampling Algorithm
 args['data_name']             = 'CIFAR10'             # Dataset
 args['header']                = True
+args['pre_trained']           = False
 
 # TODO : Measuring Metrics
 num_repeats = 1
@@ -30,3 +31,12 @@ train_loss_total = []
 validation_loss_total = []
 train_acc_total = []
 validation_acc_total = []
+
+# TODO : Time
+train_model_time           = np.zeros(num_repeats)
+generate_samples_time      = np.zeros(num_repeats)
+train_classification_time  = np.zeros(num_repeats)
+prediction_time            = np.zeros(num_repeats)
+
+time_df = pd.DataFrame(columns=['Train model time', 'Generate samples time', 'Train classification time', 'Prediction time'])
+     
