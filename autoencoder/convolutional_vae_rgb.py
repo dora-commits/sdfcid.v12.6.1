83f2115c550af56a933a837d7fa3328d7b3b46b8
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-04-12 07:01:33 +0700

    added cvae with rgb

---
+++ autoencoder/convolutional_vae_rgb.py
@@ -0,0 +1,91 @@
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super(Encoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # convolutional filters, work excellent with image data
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.n_channel, self.dim_h, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h, self.dim_h * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 2, self.dim_h * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 4, self.dim_h * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(self.dim_h * 8),
+            nn.LeakyReLU(0.2, inplace=True))
+
+        # final layer is fully connected
+        self.fc1 = nn.Linear(self.dim_h * (2 ** 3), self.n_z)
+        self.fc2 = nn.Linear(self.dim_h * (2 ** 3), self.n_z)
+
+    def reparameterize(self, mu, logvar):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size())
+
+        std = std.to(device)
+        esp = esp.to(device)
+
+        z = mu + std * esp
+        return z
+
+    # designation of z, mu, and logvar similar to PyTorch VAE example
+    # implementation on github (PyTorch 2018)
+    def bottleneck(self, h):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        mu, logvar = self.fc1(h), self.fc2(h)
+
+        mu = mu.to(device)
+        logvar = logvar.to(device)
+
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+    def forward(self, x):
+        h = self.conv(x)
+        h = h.squeeze()
+        z, mu, logvar = self.bottleneck(h)
+
+        return z, mu, logvar
+
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # first layer is fully connected
+        self.fc = nn.Sequential(
+            nn.Linear(self.n_z, self.dim_h * 8 * 9 * 9),
+            nn.ReLU()
+        )
+
+        # deconvolutional filters, essentially the inverse of convolutional filters
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(self.dim_h * 8, self.dim_h * 4, 4),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 4, self.dim_h * 2, 4),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 2, self.n_channel, 4, stride=2),
+            nn.Tanh()
+            # nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, self.dim_h * 8, 9, 9)
+        x = self.deconv(x)
+        return x
