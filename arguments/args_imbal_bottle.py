f3f0f8870c4bf254310d74cbfae296d01171febc
Pham Truong Giang <ptgiang.dora@gmail.com>
2024-04-18 01:20:29 +0700

    added args on industrial defect (bottle) dataset with imbal

---
+++ arguments/args_imbal_bottle.py
@@ -0,0 +1,44 @@
+# TODO : Arguments
+args = {}
+args['dim_h']                 = 64                    
+args['n_channel']             = 3                     # number of channels in the input data
+args['n_z']                   = 800                   
+args['lr']                    = 0.0002                # learning rate for Adam optimizer .0002
+args['epochs']                = 100                   
+args['batch_size']            = 21                    
+args['save']                  = True                  # save weights at each epoch of training if True
+args['train']                 = True                  # train networks if True, else load networks from
+args['resnet_learning_rate']  = 0.001                 # learning rate for Adam optimizer .001 with resnet18
+args['resnet_num_epochs']     = 20                    # how many epochs to run for
+args['oversampling_method']   = 'IMBALANCE'           # Oversampling Algorithm
+args['data_name']             = 'MVTECAD'             # Dataset
+args['header']                = True
+args['sigma']                 = 1.0
+args['lambda']                = 0.01
+args['pre_trained']           = True
+
+# TODO : Measuring Metrics
+num_repeats = 1
+Accuracy      = np.zeros(num_repeats)
+GeometricMean = np.zeros(num_repeats)
+F1Score       = np.zeros(num_repeats)
+Precision     = np.zeros(num_repeats)
+Recall        = np.zeros(num_repeats)
+
+result_df = pd.DataFrame(columns=['Accuracy', 'GeometricMean', 'F1Score', 'Precision', 'Recall'])
+
+# TODO : Check overfitting problem
+train_loss_total = []
+validation_loss_total = []
+train_acc_total = []
+validation_acc_total = []
+
+
+# TODO : Time
+train_model_time           = np.zeros(num_repeats)
+generate_samples_time      = np.zeros(num_repeats)
+train_classification_time  = np.zeros(num_repeats)
+prediction_time            = np.zeros(num_repeats)
+
+time_df = pd.DataFrame(columns=['Train model time', 'Generate samples time', 'Train classification time', 'Prediction time'])
+     
